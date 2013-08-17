1c4c0fee737d73802f1329144aeda29b22ca05d2
Pablo Moyano <ultraklon@gmail.com>
2013-08-17 00:53:03 -0300

    Trying to fix issue #2

--- tres.js
+++ tres.js
@@ -1,16 +1,21 @@
 var request = require('request'),
+  Iconv  = require('iconv').Iconv,
+  ent = require('ent'),
   parse = require('./parseHtml'),
+  http = require('http'),
   mongoose = require('mongoose');
 
 
 var dbConnection=connectToDB("cedom1", function(db){
   //initial("http://www.cedom.gov.ar/es/legislacion/normas/leyes/ley4576.html", "./leycedom.json");
-  initial("http://www.cedom.gov.ar/es/legislacion/normas/leyes/index.php?pagina=9", "./listaleyes.json", function(leyes){ listCallback(leyes, db)});
+  initial("www.cedom.gov.ar", "/es/legislacion/normas/leyes/index.php?pagina=9", "./listaleyes.json", function(leyes){ listCallback(leyes, db)});
+  //initial("www.cedom.gov.ari", "/es/legislacion/normas/leyes/index.php?pagina=9", "./listaleyes.json", function(leyes){ listCallback(leyes, db)});
+  //initial("http://www.cedom.gov.ar/es/legislacion/normas/leyes/index.php?pagina=9", "./listaleyes.json", function(leyes){ listCallback(leyes, db)});
 });
 
-function initial(url, regexConfigFilename, outputCallBack){
+function initial(url, path, regexConfigFilename, outputCallBack){
   var jsonRegex=readStructure(regexConfigFilename);
-  traerPag(url, function(body){
+  traerPag(url, path, function(body){
     parse(body, jsonRegex, function(leyes){
       outputCallBack(leyes, dbConnection);
     })
@@ -19,8 +24,15 @@ function initial(url, regexConfigFilename, outputCallBack){
 
 function listCallback(leyes, dbConnection){
   for(ley in leyes){
+    var url1=leyes[ley].url.split('\/')[2];
+    var path1="";
+    var i=3;
+    while(leyes[ley].url.split('\/')[i]){
+      path1+="/"+leyes[ley].url.split('\/')[i];
+      i++;
+    }
     try{
-      initial(leyes[ley].url, "./leycedom.json", function(data){writerCallback(data, dbConnection)});
+      initial(url1, path1, "./leycedom.json", function(data){writerCallback(data, dbConnection)});
     }catch(e){
       console.log("Error parsing "+leyes[ley].url);
     }
@@ -68,21 +80,29 @@ function writerCallback(writeEntity, dbConnection){
   if(writeEntity){
     var entityInstance = new Entity(writeEntity);
     entityInstance.save(function(){console.log('saved')});
-
-    console.log("Escribiendo: " + JSON.stringify(writeEntity));
   }
 }
 
-function traerPag(host, callback){
-  request(host, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+function traerPag(host, path, callback){
+  var options = {
+      host: host
+    , path: path
+    , port: 80
+  }
+
+  var request = http.get(options, function(res){
+    var body="";
+    res.setEncoding('ascii');
+    res.on('end', function(){
       callback(body);
-    }
+    });
+    res.on('data', function(chunk){
+      body += chunk;
+    });
   });
+
 }
 
 function readStructure(fileName){
   return require(fileName);
 }
-
-
