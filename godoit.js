5b1ad61c3643e7f22d65797ee34cebe5ab09b5a7
Pablo Moyano <ultraklon@gmail.com>
2013-09-20 01:07:11 -0300

    almost saving

--- godoit.js
+++ godoit.js
@@ -1,4 +1,5 @@
-var Browser = require("zombie")
+var Browser = require("zombie"),
+  mongoose = require('mongoose');
 
 function main(){
   var browser = new Browser({ debug: true })
@@ -36,8 +37,36 @@ function processPage(browser){
       }
     } 
   }
-  showArticles(expediente, comision, articles);
+  //showArticles(expediente, comision, articles);
+  saveAll(expediente, comision, articles);
 }
+
+function saveAll(expediente, comision, articles){
+  connectToDB("cedom2", expediente, comision, articles, function(db, expediente, comision, articles){
+    showArticles(expediente, comision, articles);
+    /*var obj=new Object();
+    obj['expediente']=expediente;
+    obj['comision']=comision;
+    obj['articles']=articles;
+    writerCallback(obj, db);*/
+  });
+}
+
+function writerCallback(writeEntity, dbConnection){
+  if(writeEntity){
+    var entityInstance = new Entity(writeEntity);
+    console.log('saving: '+ writeEntity.expediente );
+    entityInstance.save(function(){console.log('saved: '+ writeEntity.expediente )});
+  }
+}
+
+function createEntity(){
+  var schemaLocal='{ "expediente" : "String", "comision" : "String", "articles" : ["String"] }';
+  var entityTemp=JSON.parse(schemaLocal);
+  var entitySchema = mongoose.Schema(entityTemp);
+  Entity = mongoose.model("entities", entitySchema);
+}
+
 
 //shows articles, just for debugging purposes
 function showArticles(expediente, comision, articles){
@@ -50,6 +79,15 @@ function showArticles(expediente, comision, articles){
     console.log("--------------------");
     console.log(articles[a]);
   }
+}
+
+function connectToDB(dbName, expediente, comision, articles, callback){
+  mongoose.connect('mongodb://localhost/'+dbName);
+  createEntity();
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  //db.once('open', callback(db, expediente, comision, articles));
+  db.once('open', function(db, expediente, comision, articles){callback(db, expediente, comision, articles)});
 }
 
 //removes html from text and replaces new lines with a blank space 
